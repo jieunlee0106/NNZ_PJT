311a90cb99cbec645c13edc694f695256f95b8db
김지환 <wlghks960@naver.com>
2023-05-03 15:36:44 +0900

    FEAT : 나눔등록 api 작성

--- fe/user/nnz/lib/src/services/sharing_register.dart
+++ fe/user/nnz/lib/src/services/sharing_register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:logger/logger.dart';
+import 'package:nnz/src/model/share_model.dart';
 
 class SharingRegisterProvider extends GetConnect {
   final logger = Logger();
@@ -18,38 +19,82 @@ class SharingRegisterProvider extends GetConnect {
 
   //공연 목록 검색
   Future<Response> getShowList({required String category}) async {
-    try {
-      final response = await get("/show?=$category", headers: headers);
-      if (response.statusCode == 200) {
-        return response.body;
-      } else {
-        final errorMessage = "(${response.statusCode}): ${response.body}";
-        logger.e(errorMessage);
-        throw Exception(errorMessage);
-      }
-    } catch (e) {
-      final errorMessage = "$e";
-      logger.e(errorMessage);
-      throw Exception(errorMessage);
-    }
+    // try {
+    //   final response = await get("/show?=$category", headers: headers);
+    //   if (response.statusCode == 200) {
+    //     return response.body;
+    //   } else {
+    //     final errorMessage = "(${response.statusCode}): ${response.body}";
+    //     logger.e(errorMessage);
+    //     throw Exception(errorMessage);
+    //   }
+    // } catch (e) {
+    //   final errorMessage = "$e";
+    //   logger.e(errorMessage);
+    //   throw Exception(errorMessage);
+    // }
+
+    final response = await get('/show?=$category', headers: headers);
+    return response;
   }
 
   //카테고리 조회
   Future<Response> getCategory({required String parent}) async {
-    try {
-      final response =
-          await get("/shows/categories?=$parent", headers: headers);
-      if (response.statusCode == 200) {
-        return response.body;
-      } else {
-        final errorMessage = "(${response.statusCode}): ${response.body}";
-        logger.e(errorMessage);
-        throw Exception(errorMessage);
-      }
-    } catch (e) {
-      final errorMessage = "$e";
-      logger.e(errorMessage);
-      throw Exception(errorMessage);
+    // try {
+    //   final response =
+    //       await get("/shows/categories?=$parent", headers: headers);
+    //   if (response.statusCode == 200) {
+    //     return response.body;
+    //   } else {
+    //     final errorMessage = "(${response.statusCode}): ${response.body}";
+    //     logger.e(errorMessage);
+    //     throw Exception(errorMessage);
+    //   }
+    // } catch (e) {
+    //   final errorMessage = "$e";
+    //   logger.e(errorMessage);
+    //   throw Exception(errorMessage);
+    // }
+
+    final response = await get("/shows/categories?=$parent", headers: headers);
+    return response;
+  }
+
+  //나눔등록
+  void testShare({required ShareModel shareModel, required var images}) async {
+    final body = shareModel.toJson();
+    var formData = FormData(body);
+
+    for (var i = 0; i < images.length; i++) {
+      formData.files.add(
+        MapEntry(
+          'images',
+          MultipartFile(images[i].path, filename: images[i].name),
+        ),
+      );
     }
+    // for (var element in formData.files) {
+    //   logger.i(element.toString());
+    // }
+    // logger.i("들어왔습니다. ${formData.files[0].value}");
+  }
+
+  //나눔등록 api
+  Future<Response> postShare(
+      {required ShareModel shareModel, required var images}) async {
+    final body = shareModel.toJson();
+    var formData = FormData(body);
+    for (var i = 0; i < images.length; i++) {
+      formData.files.add(
+        MapEntry(
+          'images',
+          MultipartFile(images[i].path, filename: images[i].name),
+        ),
+      );
+    }
+
+    final response =
+        await post("/nanums", formData, contentType: 'multipart/form-data');
+    return response;
   }
 }
