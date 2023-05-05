820ee91a4a66d215d04376bbbfb365989f3a29b5
wlghks960 <98100778+wlghks960@users.noreply.github.com>
2023-05-05 19:58:01 +0900

    FEAT : 인기해시태그 API 통신 후 리스트 생성

--- fe/user/nnz/lib/src/controller/search_controller.dart
+++ fe/user/nnz/lib/src/controller/search_controller.dart
@@ -10,14 +10,37 @@ class ShowSearchController extends GetxController {
   final logger = Logger();
   RxString category = ''.obs;
   RxBool hasFocus = false.obs;
-
+  List<String> tagList = [];
   @override
-  void onInit() {
+  void onInit() async {
     // TODO: implement onInit
 
     searchController = TextEditingController();
+    await onPopularTag();
   }
 
+  //인기 해시 태그
+  Future<List<String>> onPopularTag() async {
+    try {
+      final response = await SearchProvider().getPopularTag();
+      if (response.statusCode == 200) {
+        for (var tag in response.body) {
+          tagList.add(tag);
+        }
+        return tagList;
+      } else {
+        final errorMessage = "(${response.statusCode}): ${response.body}";
+        logger.e(errorMessage);
+        throw Exception(errorMessage);
+      }
+    } catch (e) {
+      final errorMessage = "$e";
+      logger.e(errorMessage);
+      throw Exception(errorMessage);
+    }
+  }
+
+  //나눔 및 검색
   Future<void> onChangeCategory(
       {required String text, required String type}) async {
     searchText(text);
