862a43a9f5b23fc388d149977f1746d668aceef3
jongseong <eello.bdev@gmail.com>
2023-05-09 17:21:15 +0900

    FIX: 사용자 신고 예외, 로거 추가

--- be/user-service/src/main/java/nnz/userservice/service/impl/AskServiceImpl.java
+++ be/user-service/src/main/java/nnz/userservice/service/impl/AskServiceImpl.java
@@ -1,11 +1,15 @@
 package nnz.userservice.service.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.github.eello.nnz.common.exception.CustomException;
 import io.github.eello.nnz.common.kafka.KafkaMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nnz.userservice.dto.ShowRegisterVO;
 import nnz.userservice.dto.UserReportVO;
+import nnz.userservice.entity.User;
+import nnz.userservice.exception.ErrorCode;
+import nnz.userservice.repository.UserRepository;
 import nnz.userservice.service.AskService;
 import nnz.userservice.service.KafkaProducer;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AskServiceImpl implements AskService {
 
+    private final UserRepository userRepository;
     private final KafkaProducer kafkaProducer;
 
     @Override
@@ -25,7 +30,13 @@ public class AskServiceImpl implements AskService {
 
     @Override
     public void reportUser(UserReportVO vo) throws JsonProcessingException {
+        if (userRepository.findById(vo.getTargetId()).isEmpty()) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        }
+
         KafkaMessage kafkaMessage = KafkaMessage.create().body(vo);
         kafkaProducer.sendMessage("report", kafkaMessage);
+
+        log.info("유저 식별자 {}번님이 {}님을 신고", vo.getReporterId(), vo.getTargetId());
     }
 }
