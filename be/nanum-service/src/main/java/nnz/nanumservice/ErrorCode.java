6b9670094c5f8224a863283ad32be8e4cd34c37b
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-05-10 17:08:47 +0900

    FEAT: 받은 나눔 인증 처리

--- be/nanum-service/src/main/java/nnz/nanumservice/ErrorCode.java
+++ be/nanum-service/src/main/java/nnz/nanumservice/ErrorCode.java
@@ -5,8 +5,7 @@ import io.github.eello.nnz.common.exception.AbstractErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @RequiredArgsConstructor
 public enum ErrorCode implements AbstractErrorCode {
@@ -15,7 +14,9 @@ public enum ErrorCode implements AbstractErrorCode {
     SHOW_NOT_FOUND("S001", "등록되지 않은 공연입니다.", NOT_FOUND),
     CATEGORY_NOT_FOUND("C001", "등록되지 않은 카테고리입니다.", NOT_FOUND),
     NANUM_NOT_FOUND("N001", "등록되지 않은 나눔입니다.", NOT_FOUND),
+    NANUM_NOT_ACTIVE("N003", "진행중인 나눔이 아닙니다.", BAD_REQUEST),
     USER_NANUM_NOT_FOUND("UN002", "등록되지 않은 받은 나눔입니다.", NOT_FOUND),
+    ALREADY_USER_NANUM("UN003", "이미 처리한 나눔 인증입니다.", CONFLICT),
 
     ;
 
--- be/nanum-service/src/main/java/nnz/nanumservice/service/impl/CertificationServiceImpl.java
+++ be/nanum-service/src/main/java/nnz/nanumservice/service/impl/CertificationServiceImpl.java
@@ -30,6 +30,9 @@ public class CertificationServiceImpl implements CertificationService {
         UserNanum userNanum = userNanumRepository.findById(nanumCertificationVO.getId())
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NANUM_NOT_FOUND));
 
+        // 이미 처리한 인증인지 확인
+        if(userNanum.getIsCertificated() != null) throw new CustomException(ErrorCode.ALREADY_USER_NANUM);
+
         // true : 인증 성공, false : 인증 실패
         userNanum.updateIsCertificated(nanumCertificationVO.getCertification());
     }
