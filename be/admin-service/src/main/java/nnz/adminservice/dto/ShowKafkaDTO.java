57f363e946df24bce313addb0aaf1383d0646fed
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-05-10 17:29:40 +0900

    FIX: ShowKafkaDTO 수정

--- be/admin-service/src/main/java/nnz/adminservice/dto/ShowKafkaDTO.java
+++ be/admin-service/src/main/java/nnz/adminservice/dto/ShowKafkaDTO.java
@@ -1,17 +1,23 @@
 package nnz.adminservice.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import nnz.adminservice.entity.Show;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ShowKafkaDTO {
-    private String category;
+    private Long id;
 
     private String title;
 
@@ -24,19 +30,40 @@ public class ShowKafkaDTO {
     private String ageLimit;
     private String region;
 
-    private String poster;
+    private String posterImage;
+    private String categoryCode;
+
+    private Long createdBy;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime createdAt;
+
+    private Long updatedBy;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime updatedAt;
+
+    protected boolean isDelete;
+
 
     public static ShowKafkaDTO entityToDTO(Show show) {
 
         return ShowKafkaDTO.builder()
-                .category(show.getCategory().getName())
+                .categoryCode(show.getCategory().getCode())
                 .ageLimit(show.getAgeLimit())
                 .location(show.getLocation())
                 .region(show.getRegion())
-                .poster(show.getPosterImage())
+                .posterImage(show.getPosterImage())
                 .startDate(show.getStartDate())
                 .endDate(show.getEndDate())
                 .title(show.getTitle())
+                .createdBy(show.getCreatedBy())
+                .createdAt(show.getCreatedAt())
+                .updatedBy(show.getUpdatedBy())
+                .updatedAt(show.getUpdatedAt())
+                .isDelete(show.getIsDelete())
                 .build();
     }
 }
