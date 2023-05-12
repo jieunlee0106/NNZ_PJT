b859e7b2ec58c4e1fd7ce82cf4b8415d2cee5f10
김지환 <wlghks960@naver.com>
2023-05-12 10:21:22 +0900

    FEAT : 트위터 트윗 API 완료

--- fe/user/nnz/lib/src/pages/user/mypage.dart
+++ fe/user/nnz/lib/src/pages/user/mypage.dart
@@ -42,91 +42,93 @@ class _MyPageState extends State<MyPage> {
   Widget build(BuildContext context) {
     // var myInfo = controller.myInfo;
     if (_isLoading) {
-      return Center(
+      return const Center(
         child: CircularProgressIndicator(),
       );
-    }
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          iconTheme: IconThemeData(color: Colors.black),
-          title: Center(child: Image.asset(ImagePath.logo, width: 80)),
-          actions: [Icon(Icons.more_vert)],
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30),
-            child: Expanded(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      MyProfile(nickname: myInfo?.nickname),
-                      MyFollower(
-                        follower: myInfo?.followerCount,
-                        following: myInfo?.followingCount,
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  GrayLine(),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15),
-                        child: Row(
-                          children: [
-                            Image.asset(
-                              ImagePath.gift,
-                              width: 30,
-                            ),
-                            Text(
-                              '  나의 나눔',
-                              style: TextStyle(
-                                color: Config.blackColor,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w600,
+    } else {
+      return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            iconTheme: const IconThemeData(color: Colors.black),
+            title: Center(child: Image.asset(ImagePath.logo, width: 80)),
+            actions: const [Icon(Icons.more_vert)],
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: Expanded(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        MyProfile(nickname: myInfo.nickname),
+                        MyFollower(
+                          follower: myInfo.followerCount,
+                          following: myInfo.followingCount,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    GrayLine(),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Row(
+                            children: [
+                              Image.asset(
+                                ImagePath.gift,
+                                width: 30,
                               ),
-                            )
+                              Text(
+                                '  나의 나눔',
+                                style: TextStyle(
+                                  color: Config.blackColor,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 15, top: 10),
+                              child: Container(
+                                  width: 135,
+                                  height: 5,
+                                  color:
+                                      const Color.fromARGB(255, 230, 230, 230)),
+                            ),
                           ],
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 15, top: 10),
-                            child: Container(
-                                width: 135,
-                                height: 5,
-                                color: Color.fromARGB(255, 230, 230, 230)),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  SharingInfo(
-                    share: '나눔 한 내역',
-                    total: myInfo?.statistics?.nanum?.totalCount ?? 0,
-                    yet: myInfo?.statistics?.nanum?.beforeCount ?? 0,
-                    ing: myInfo?.statistics?.nanum?.ongoingCount ?? 0,
-                    end: myInfo?.statistics?.nanum?.doneCount ?? 0,
-                    page: MySharingList(),
-                  ),
-                  SharingInfo(
-                    share: '나눔 받은 내역',
-                    total: myInfo?.statistics?.receive?.totalCount ?? 0,
-                    yet: myInfo?.statistics?.receive?.totalCount ?? 0,
-                    ing: myInfo?.statistics?.receive?.totalCount ?? 0,
-                    end: myInfo?.statistics?.receive?.totalCount ?? 0,
-                    page: MySharedList(),
-                  ),
-                ],
+                      ],
+                    ),
+                    SharingInfo(
+                      share: '나눔 한 내역',
+                      total: myInfo.statistics?.nanum?.totalCount ?? 0,
+                      yet: myInfo.statistics?.nanum?.beforeCount ?? 0,
+                      ing: myInfo.statistics?.nanum?.ongoingCount ?? 0,
+                      end: myInfo.statistics?.nanum?.doneCount ?? 0,
+                      page: const MySharingList(),
+                    ),
+                    SharingInfo(
+                      share: '나눔 받은 내역',
+                      total: myInfo.statistics?.receive?.totalCount ?? 0,
+                      yet: myInfo.statistics?.receive?.totalCount ?? 0,
+                      ing: myInfo.statistics?.receive?.totalCount ?? 0,
+                      end: myInfo.statistics?.receive?.totalCount ?? 0,
+                      page: const MySharedList(),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ));
+          ));
+    }
   }
 }
