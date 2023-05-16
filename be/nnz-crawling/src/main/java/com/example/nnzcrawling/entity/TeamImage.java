902df20cc669bd7c6af65545e73b0eff361c6872
yyh7750 <yyh7750@gmail.com>
2023-05-16 21:19:38 +0900

    @Where() 어노테이션 추가 is_delete=0

--- be/nnz-crawling/src/main/java/com/example/nnzcrawling/entity/TeamImage.java
+++ be/nnz-crawling/src/main/java/com/example/nnzcrawling/entity/TeamImage.java
@@ -2,6 +2,7 @@ package com.example.nnzcrawling.entity;
 
 import io.github.eello.nnz.common.entity.BaseEntity;
 import lombok.*;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Where(clause = "is_delete = 0")
 public class TeamImage extends BaseEntity {
 
     @Id
