7d50009ea96b95092c96036670b8187047d9e411
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-05-16 10:52:27 +0900

    FEAT: PUSH 알림 여러 명에게 전송

--- be/nanum-service/src/main/java/nnz/nanumservice/service/impl/FCMService.java
+++ be/nanum-service/src/main/java/nnz/nanumservice/service/impl/FCMService.java
@@ -6,6 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import nnz.nanumservice.dto.FcmNotificationDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -26,5 +29,40 @@ public class FCMService {
                 .build();
 
         FirebaseMessaging.getInstance().send(message);
+    }
+
+    public void sendMessage(String title, String body, String token) throws FirebaseMessagingException {
+        Notification notification = Notification.builder()
+                .setTitle(title)
+                .setBody(body)
+                .build();
+
+        Message message = Message.builder()
+                .setAndroidConfig(AndroidConfig.builder()
+                        .setPriority(AndroidConfig.Priority.HIGH)
+                        .build())
+                .setToken(token)
+                .setNotification(notification)
+                .build();
+
+        FirebaseMessaging.getInstance().send(message);
+    }
+
+    public void sendMultipleMessage(String title, String body, List<String> tokens) throws FirebaseMessagingException {
+        Notification notification = Notification.builder()
+                .setTitle(title)
+                .setBody(body)
+                .build();
+
+        List<Message> messages = new ArrayList<>();
+        tokens.stream().map(t-> Message.builder()
+                .setAndroidConfig(AndroidConfig.builder()
+                        .setPriority(AndroidConfig.Priority.HIGH)
+                        .build())
+                .setToken(t)
+                .setNotification(notification)
+                .build());
+
+        FirebaseMessaging.getInstance().sendAll(messages);
     }
 }
