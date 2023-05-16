d32098b4867ccde98d1560e09a558dabddf87c2c
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-05-16 13:54:27 +0900

    FIX: FCM json파일 경로 수정

--- be/nanum-service/src/main/java/nnz/nanumservice/config/FCMInitializer.java
+++ be/nanum-service/src/main/java/nnz/nanumservice/config/FCMInitializer.java
@@ -7,6 +7,7 @@ import io.github.eello.nnz.common.exception.CustomException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
@@ -21,9 +22,10 @@ public class FCMInitializer {
 
     @PostConstruct
     public void initialize(){
-        ClassPathResource resource = new ClassPathResource("nnz-firebase.json");
+//        ClassPathResource resource = new ClassPathResource("nnz-firebase.json");
+        FileSystemResource file = new FileSystemResource("/home/fcm/nnz-firebase.json");
 
-        try(InputStream stream = resource.getInputStream()){
+        try(InputStream stream = file.getInputStream()){
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(stream))
                     .build();
