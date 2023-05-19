3d47946ca4e7d3fc826bb1e0c9fc07063841d297
yeomyuri42@gmail.com <yeomyuri42@gmail.com>
2023-05-19 12:14:16 +0900

    FIX: kafka

--- be/nanum-service/src/main/java/nnz/nanumservice/service/impl/CertificationServiceImpl.java
+++ be/nanum-service/src/main/java/nnz/nanumservice/service/impl/CertificationServiceImpl.java
@@ -2,10 +2,13 @@ package nnz.nanumservice.service.impl;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
 import io.github.eello.nnz.common.exception.CustomException;
+import io.github.eello.nnz.common.kafka.KafkaMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import nnz.nanumservice.dto.CertificationDTO;
 import nnz.nanumservice.dto.FcmNotificationDTO;
+import nnz.nanumservice.dto.NanumDTO;
+import nnz.nanumservice.dto.UserNanumDTO;
 import nnz.nanumservice.exception.ErrorCode;
 import nnz.nanumservice.entity.Nanum;
 import nnz.nanumservice.entity.NanumStock;
@@ -15,6 +18,7 @@ import nnz.nanumservice.repository.NanumStockRepository;
 import nnz.nanumservice.repository.UserNanumRepository;
 import nnz.nanumservice.repository.UserRepository;
 import nnz.nanumservice.service.CertificationService;
+import nnz.nanumservice.service.KafkaProducer;
 import nnz.nanumservice.vo.NanumCertificationVO;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,6 +38,7 @@ public class CertificationServiceImpl implements CertificationService {
     private final UserNanumRepository userNanumRepository;
     private final NanumStockRepository nanumStockRepository;
     private final FCMService fcmService;
+    private final KafkaProducer producer;
 
     @Override
     public void handleNanumCertification(Long nanumId, NanumCertificationVO nanumCertificationVO) throws FirebaseMessagingException {
@@ -49,6 +54,10 @@ public class CertificationServiceImpl implements CertificationService {
         // true : 인증 성공, false : 인증 실패
         userNanum.updateIsCertificated(nanumCertificationVO.getCertification());
 
+        UserNanumDTO userNanumDTO = UserNanumDTO.of(userNanum);
+        KafkaMessage<UserNanumDTO> userNanumDTOKafkaMessage = KafkaMessage.update().body(userNanumDTO);
+        producer.sendMessage(userNanumDTOKafkaMessage, "usernanum");
+
         int countByIsCertificated = userNanumRepository.countByNanumAndIsCertificated(nanum, true);
         if(countByIsCertificated == nanum.getQuantity()){
             nanum.updateStatus(1);
@@ -58,6 +67,9 @@ public class CertificationServiceImpl implements CertificationService {
                             .body(nanum.getIsCertification() ? "인증을 검사하러 가볼까요?" : "")
                             .userToken(nanum.getProvider().getDeviceToken())
                             .build());
+
+            KafkaMessage<NanumDTO> kafkaMessage = KafkaMessage.update().body(NanumDTO.of(nanum));
+            producer.sendMessage(kafkaMessage, "nanum");
         }
     }
 
@@ -86,7 +98,11 @@ public class CertificationServiceImpl implements CertificationService {
                 .build()));
 
         // 수량과 재고가 같다면 이제 나눔이 시작됐다는 뜻이므로 상태를 진행중으로 바꾼다.
-        if(ns.getStock() == nanum.getQuantity()) nanum.updateStatus(2);
+        if(ns.getStock() == nanum.getQuantity()) {
+            nanum.updateStatus(2);
+            KafkaMessage<NanumDTO> kafkaMessage = KafkaMessage.update().body(NanumDTO.of(nanum));
+            producer.sendMessage(kafkaMessage, "nanum");
+        }
 
         // 현재 진행중인 나눔이 아닐 경우
         if(nanum.getStatus() != 2) throw new CustomException(ErrorCode.NANUM_NOT_ACTIVE);
@@ -97,12 +113,19 @@ public class CertificationServiceImpl implements CertificationService {
         // UserNanum isReceived 상태 변경
         userNanum.updateIsReceived();
 
+        UserNanumDTO userNanumDTO = UserNanumDTO.of(userNanum);
+        KafkaMessage<UserNanumDTO> userNanumDTOKafkaMessage = KafkaMessage.update().body(userNanumDTO);
+        producer.sendMessage(userNanumDTOKafkaMessage, "usernanum");
+
         NanumStock save = nanumStockRepository.save(ns);
 
         // 재고가 0이면 나눔 종료 and 재고 0
         if(save.getStock() == 0){
             nanum.updateStatus(2);
             nanum.updateStock(0);
+
+            KafkaMessage<NanumDTO> kafkaMessage = KafkaMessage.update().body(NanumDTO.of(nanum));
+            producer.sendMessage(kafkaMessage, "nanum");
         }
 
         // 남은 수량 return
